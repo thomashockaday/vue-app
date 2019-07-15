7a98837aba8c2a79f5c46340974592f933235312
Thomas Hockaday <thomashockaday95@gmail.com>
2019-07-15 15:58:00 +0100

    Build employee app based on https://www.taniarascia.com/getting-started-with-vue/

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <section class="section">
+        <div id="app" class="container">
+            <h1 class="title">Employees</h1>
+
+            <employee-form @add:employee="addEmployee" />
+            <employee-table
+                :employees="employees"
+                @delete:employee="deleteEmployee"
+                @edit:employee="editEmployee"
+            />
+        </div>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import EmployeeTable from '@/components/EmployeeTable.vue';
+    import EmployeeForm from '@/components/EmployeeForm.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            EmployeeTable,
+            EmployeeForm,
+        },
+        data() {
+            return {
+                employees: [
+                    {
+                        id: 1,
+                        name: 'Richard Hendricks',
+                        email: 'richard@piedpiper.com',
+                    },
+                    {
+                        id: 2,
+                        name: 'Bertram Gilfoyle',
+                        email: 'gilfoyle@piedpier.com',
+                    },
+                    {
+                        id: 3,
+                        name: 'Dinesh Yugati',
+                        email: 'dineshop@piedpiper.som'
+                    },
+                ],
+            };
+        },
+        methods: {
+            addEmployee(employee) {
+                const lastId =
+                    this.employees.length > 0
+                         ? this.employees[this.employees.length - 1].id
+                         : 0;
+                const id = lastId + 1;
+                const newEmployee = { ...employee, id };
+
+                this.employees = [...this.employees, newEmployee];
+            },
+            deleteEmployee(id) {
+                this.employees = this.employees.filter(employee => employee.id !== id);
+            },
+            editEmployee(id, updatedEmployee) {
+                this.employees = this.employees.map(employee =>
+                    employee.id === id ? updatedEmployee : employee
+                );
+            },
+        },
+    };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
